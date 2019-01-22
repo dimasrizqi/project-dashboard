b57ad5ed6f5a921cd1ebc47a069a8a683552e5f4
dimasrizqi <dimasrizqi3128@yahoo.co.id>
2019-01-22 09:18:51 +0700

    testing testing

--- asset/admin.py
+++ asset/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Assetmaster, License, Device
-
+#hahhaha
 # Register your models here.
 admin.site.register(Assetmaster)
 admin.site.register(Device)
